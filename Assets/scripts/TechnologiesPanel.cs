337663fb9dae8a99c893c6dea6f3e2756617a440
charrinton <charrinton@gmail.com>
2015-11-18 19:33:32 +0100

    Added UI for Technologies

---
+++ Assets/scripts/TechnologiesPanel.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TechnologiesPanel : MonoBehaviour {
+
+	public GameObject technologyDetailPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable(){
+		technologyDetailPanel.SetActive (false);
+	}
+}
---
+++ Assets/scripts/TechnologyDetailPanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TechnologyDetailPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OpenConfirmResearchPanel(int technologyId){
+		GetComponentInParent <GameObject> ().SetActive (true);
+	}
+}
