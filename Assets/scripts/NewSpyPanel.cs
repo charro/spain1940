b9afa6283618b11c752d99938495c8adbcf4e6a2
riverdevs <riverdevs@gmail.com>
2016-01-13 18:15:07 +0100

    Restrict Spy Level if not researched yet

---
+++ Assets/scripts/NewSpyPanel.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NewSpyPanel : MonoBehaviour {
+
+	public int spyLevel;
+	public Text levelText;
+	public Button newSpyButton;
+	public Text descriptionText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnEnable(){
+		newSpyButton.interactable = IsThisLevelResearched();
+	}
+
+	private bool IsThisLevelResearched(){
+		ResearchManager researchManager = FindObjectOfType<ResearchManager> ();
+		bool researched = false;
+
+		switch (spyLevel) {
+			case 1:
+				researched = researchManager.IsAlreadyResearched (TechnologyType.Spying1);
+				break;
+			case 2:
+				researched =  researchManager.IsAlreadyResearched (TechnologyType.ArmourPlate);
+				break;
+			case 3:
+				researched =  researchManager.IsAlreadyResearched (TechnologyType.ArmourPlate);
+				break;
+		}
+
+		return researched;
+	}
+}
