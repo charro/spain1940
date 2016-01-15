ee2ac86d782353f12709d027cc8c5df631f02485
riverdevs <riverdevs@gmail.com>
2016-01-15 10:01:15 +0100

    First working Unit Test

--- Assets/editor/CombatTest.cs
+++ Assets/editor/CombatTest.cs
@@ -1,31 +1,56 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 public class CombatTest {
 
     [Test]
     public void CombatManagerTest()
     {
+		Dictionary<ArmyType, int> attackerArmyDefinition = new Dictionary<ArmyType, int> ();
+		attackerArmyDefinition.Add (ArmyType.Milicia, 31);
+		attackerArmyDefinition.Add (ArmyType.TankBisonte, 66);
+
+		Dictionary<ArmyType, int> defenderArmyDefinition = new Dictionary<ArmyType, int> ();
+		defenderArmyDefinition.Add (ArmyType.Milicia, 32);
+		defenderArmyDefinition.Add (ArmyType.TankBisonte, 66);
+
         //Create all components
 		GameObject combatManager = new GameObject("mockCombatManager");
 		combatManager.AddComponent<CombatManager>();
 
-		GameObject attackerRegion = new GameObject("attackerRegion");
-		attackerRegion.AddComponent<Region>();
-		GameObject defenderRegion = new GameObject("defenderRegion");
-		defenderRegion.AddComponent<Region>();
+		GameObject attackerRegionGameObject = new GameObject("attackerRegion");
+		attackerRegionGameObject.AddComponent<Region>();
+		GameObject defenderRegionGameObject = new GameObject("defenderRegion");
+		defenderRegionGameObject.AddComponent<Region>();
 
-		attackerRegion.GetComponent<Region> ().AddUnitsToArmy (ArmyType.Milicia, 33);
-		attackerRegion.GetComponent<Region> ().AddUnitsToArmy (ArmyType.TankBisonte, 15);
+		Region attackerRegion = attackerRegionGameObject.GetComponent<Region> ();
+		Region defenderRegion = defenderRegionGameObject.GetComponent<Region> ();
 
-		defenderRegion.GetComponent<Region> ().AddUnitsToArmy (ArmyType.TankToro, 13);
-		defenderRegion.GetComponent<Region> ().AddUnitsToArmy (ArmyType.TankLince, 1);
+		// Initialize the army of each region
+		int maxSlots = attackerRegion.GetMaxSlots();
+		attackerRegion.armySlots = new RegionArmySlot[maxSlots];
+		attackerRegion.ClearArmySlots ();
+		defenderRegion.armySlots = new RegionArmySlot[maxSlots];
+		defenderRegion.ClearArmySlots ();
 
+		foreach(KeyValuePair<ArmyType, int> army in attackerArmyDefinition)
+		{
+			attackerRegion.AddUnitsToArmy (army.Key, army.Value);
+		}
+
+		foreach(KeyValuePair<ArmyType, int> army in defenderArmyDefinition)
+		{
+			defenderRegion.AddUnitsToArmy (army.Key, army.Value);
+		}
+
+		// Create and init a new Combat Manager
 		CombatManager combatManagerComponent = combatManager.GetComponent<CombatManager> ();
-		combatManagerComponent.SetAttackerRegion (attackerRegion.GetComponent<Region> ());
-		combatManagerComponent.SetDefenderRegion (defenderRegion.GetComponent<Region> ());
+		combatManagerComponent.SetAttackerRegion (attackerRegion);
+		combatManagerComponent.SetDefenderRegion (defenderRegion);
 
+		// Start the combat
 		combatManagerComponent.CalculateCombat ();
 
 		Assert.Pass ();
