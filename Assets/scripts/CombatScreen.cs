05988cd3d7fdbba205e1b87309084708d8735b9e
riverdevs <riverdevs@gmail.com>
2016-01-16 19:54:55 +0100

    Fixed combat screen

--- Assets/scripts/CombatScreen.cs
+++ Assets/scripts/CombatScreen.cs
@@ -17,12 +17,21 @@ public class CombatScreen : MonoBehaviour {
 	}
 
 	public void SetCombatRegions(Region republican, Region nazi){
+		foreach(CombatUnit combatUnit in republicanCombatUnits){
+			combatUnit.gameObject.SetActive (false);
+		}
+		foreach(CombatUnit combatUnit in naziCombatUnits){
+			combatUnit.gameObject.SetActive (false);
+		}
+
 		for (int i = 0; i < republican.armySlots.Length && i < republicanCombatUnits.Length; i++) {
 			republicanCombatUnits [i].SetAssociatedArmySlot (republican.armySlots[i]);
+			republicanCombatUnits [i].gameObject.SetActive (true);
 		}
 
 		for (int i = 0; i < nazi.armySlots.Length && i < naziCombatUnits.Length; i++) {
 			naziCombatUnits [i].SetAssociatedArmySlot (nazi.armySlots[i]);
+			naziCombatUnits [i].gameObject.SetActive (true);
 		}
 	}
 }
