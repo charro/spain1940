137a635e5733f94757f0b65798236b231698a34d
charrinton <charrinton@gmail.com>
2017-01-11 15:09:54 +0100

    Added sprites for spy Fixed Technologies screen

--- Assets/scripts/LevelManager.cs
+++ Assets/scripts/LevelManager.cs
@@ -5,14 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
 
+	public GameObject cover;
+	public GameObject canvas;
+	public bool coverClicked;
+
 	// Use this for initialization
 	void Start () {
-		
+		coverClicked = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (!coverClicked && 
+			(Input.touchCount > 0 || Input.GetMouseButtonUp(0))
+		   ) {
+			coverClicked = true;
+			cover.gameObject.SetActive (false);
+			canvas.gameObject.SetActive (true);
+		}
 	}
 
 	public void changeScene(string sceneName){
