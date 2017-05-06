4e93cacf8afd1d4f63724d938dab8659e999fecc
charrinton <charrinton@gmail.com>
2017-05-07 00:25:54 +0200

    Fixed bug with repeating animations when enabling

---
+++ Assets/scripts/RestartAnimationOnEnable.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartAnimationOnEnable : MonoBehaviour {
+
+	public void OnEnable(){
+		GetComponent<Animator>().SetTrigger ("TriggerAnim");
+	}
+}
